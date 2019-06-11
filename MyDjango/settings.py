2385650e269bbd9343a4da14ef362f4736b84841
麦典威 <cctvjiatao@163.com>
2019-06-12 07:53:59 +0800

    test templates

--- MyDjango/settings.py
+++ MyDjango/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MyDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'index/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
