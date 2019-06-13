1cb324380e8fed7469bc0a09fa203395ffd3dec1
麦典威 <cctvjiatao@163.com>
2019-06-13 22:20:38 +0800

    url with variable

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<year>/<int:month>/<slug:day>', views.mydate),
+    re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html', views.re_mydate),
 ]
--- index/views.py
+++ index/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world")
+
+
+# http://127.0.0.1:8000/2019/06/13
+def mydate(request, year, month, day):
+    return HttpResponse(str(year) + '/' + str(month) + '/' + str(day))
+
+
+# http://127.0.0.1:8000/2019/06/14.html
+def re_mydate(request, year, month, day):
+    return HttpResponse(str(year) + '/' + str(month) + '/' + str(day))
