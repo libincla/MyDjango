9ade643775c14c48d4d76ec713923a1e131f781b
麦典威 <cctvjiatao@163.com>
2019-06-13 22:41:36 +0800

    url with extra-variable

--- index/urls.py
+++ index/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<year>/<int:month>/<slug:day>', views.mydate),
     re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html', views.re_mydate),
     re_path('(?P<year>[0-9]{4}).html', views.myyear, name='myyear'),
+    re_path('dict/(?P<year>[0-9]{4}).htm', views.myyear_dict, {'month': '05'}, name='myyear_dict'),
 ]
--- index/views.py
+++ index/views.py
@@ -16,5 +16,11 @@ def re_mydate(request, year, month, day):
     return HttpResponse(str(year) + '/' + str(month) + '/' + str(day))
 
 
+# http://127.0.0.1:8000/2012.html
 def myyear(request, year):
     return render(request, 'myyear.html')
+
+
+# http://127.0.0.1:8000/dict/2012.htm
+def myyear_dict(request, year, month):
+    return render(request, 'myyear_dict.html', {'month': month})
