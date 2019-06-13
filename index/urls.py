cba2df7fae7420521bebb90b522d7a85ca72c4d9
麦典威 <cctvjiatao@163.com>
2019-06-13 22:30:00 +0800

    url is named

--- index/urls.py
+++ index/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index),
     path('<year>/<int:month>/<slug:day>', views.mydate),
     re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html', views.re_mydate),
+    re_path('(?P<year>[0-9]{4}).html', views.myyear, name='myyear'),
 ]
--- index/views.py
+++ index/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
@@ -13,3 +14,7 @@ def mydate(request, year, month, day):
 # http://127.0.0.1:8000/2019/06/14.html
 def re_mydate(request, year, month, day):
     return HttpResponse(str(year) + '/' + str(month) + '/' + str(day))
+
+
+def myyear(request, year):
+    return render(request, 'myyear.html')
