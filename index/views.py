b953629ec06800fcaccc2a3a3c4101c35fcba929
麦典威 <cctvjiatao@163.com>
2019-06-15 23:10:21 +0800

    create the model of Product and show data in index.html

--- index/views.py
+++ index/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
+from .models import Product
 
 
 def index(request):
-    return render(request, 'index.html', context={'title': '首页'}, status=500)
+    type_list = Product.objects.values('type').distinct()
+    name_list = Product.objects.values('name', 'type')
+    context = {'title': '首页', 'type_list': type_list, 'name_list': name_list}
+    return render(request, 'index.html', context=context, status=200)
 
 
 def login(request):
