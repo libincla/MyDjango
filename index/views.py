ebdaabb3fc781be6cddd500b5ca00b4495c1c378
麦典威 <cctvjiatao@163.com>
2019-06-15 22:47:10 +0800

    test redirect() and optimize index.html

--- index/views.py
+++ index/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return render(request, 'index.html', context={'title': '首页'}, status=500)
+
+
+def login(request):
+    # 相对路径
+    return redirect('/')
+    # 绝对路径
+    # return redirect('http://127.0.0.1:8000/')
