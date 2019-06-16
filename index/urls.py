c06f7c466f82c0b48f5bff7f1c9efe05be5ba3f1
麦典威 <cctvjiatao@163.com>
2019-06-16 20:13:05 +0800

    test ListView with Args

--- index/urls.py
+++ index/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('login.html', views.login),
-    path('index/', views.ProductList.as_view())
+    path('index/', views.ProductList.as_view()),
+    path('index/<id>.html', views.ProductListWithArgs.as_view(), {'name': 'phone'}),
 ]
--- index/views.py
+++ index/views.py
@@ -47,3 +47,26 @@ class ProductList(ListView):
         context = super().get_context_data(**kwargs)
         context['name_list'] = Product.objects.values('name', 'type')
         return context
+
+
+# http://127.0.0.1:8000/index/2.html
+class ProductListWithArgs(ListView):
+    # 设置 HTML 模板的变量名称
+    context_object_name = 'type_list'
+    # 设定模板名称
+    template_name = 'index.html'
+
+    # 重写查询函数 get_queryset，查询结果会赋值给 context_object_name所设置的变量
+    def get_queryset(self):
+        print(self.kwargs['id'])
+        print(self.kwargs['name'])
+        print(self.request.method)
+        type_list = Product.objects.values('type').distinct()
+        return type_list
+
+    # 添加其他变量
+    # 重写函数 get_context_data，该函数设置 HTML 模板的其他变量
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name_list'] = Product.objects.values('name', 'type')
+        return context
