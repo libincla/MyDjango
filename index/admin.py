adbca707d8ea9df6712a3284ae8848a7c435ddcd
麦典威 <cctvjiatao@163.com>
2019-06-19 22:08:49 +0800

    bisic showing of Admin

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+# 将index定义的模型展示在Admin后台系统中
+
+# 方法1：将模型直接注册到admin后台
+# admin.site.register(Product)
+
+# 方法2：自定义 ProductAdmin 类并继承 ModelAdmin
+# 注册方法1：使用 Python 装饰器将 ProductAdmin 和模型 Product 绑定并注册到后台
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     # 设置显示的字段
+#     list_display = ['id', 'name', 'weight', 'size', 'type']
+# 注册方法2：
+class ProductAdmin(admin.ModelAdmin):
+    # 设置显示的字段
+    list_display = ['id', 'name', 'weight', 'size', 'type']
+admin.site.register(Product, ProductAdmin)
