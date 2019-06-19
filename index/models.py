62c1b7e9be1256fcd935a30dd235da6a5ab55ba3
麦典威 <cctvjiatao@163.com>
2019-06-19 09:15:46 +0800

    test Choices with Model's return value

--- index/models.py
+++ index/models.py
@@ -6,6 +6,10 @@ class Type(models.Model):
     id = models.AutoField(primary_key=True)
     type_name = models.CharField(max_length=20)
 
+    # 设置返回值，若不设置，则默认返回Type对象。可使下拉框显示字段 type_name
+    def __str__(self):
+        return self.type_name
+
 
 # 创建产品信息表
 class Product(models.Model):
