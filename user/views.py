e612be97de8267f67c09e73c8511f68364ef698f
麦典威 <cctvjiatao@163.com>
2019-06-22 20:07:38 +0800

    test auth logout

--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.hashers import make_password
 
 
@@ -77,4 +77,5 @@ def setpasswordView(request):
 
 
 def logoutView(request):
-    pass
+    logout(request)
+    return redirect('/')
